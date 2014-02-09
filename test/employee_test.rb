04eda07ca9dbe82e025254291443cb57bd1c12d2
Jaime Bellmyer <online@bellmyer.com>
2014-02-09 17:49:37 -0600

    add describe blocks

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -1,57 +1,58 @@
 require_relative "../lib/employee"
 
+def describe label
+  @label = label
+  yield
+end
+
 def assert message
   employee = Employee.new
-  raise message unless yield(employee)
-end
-
-########
-
-puts "testing #male?"
-
-assert "should be true when gender is 'M'" do |employee|
-  employee.gender = 'M'
-  employee.male?
-end
-
-assert "should be false when gender is 'F'" do |employee|
-  employee.gender = 'F'
-  !employee.male?
-end
-
-########
-
-puts "testing #female?"
-
-assert "should be false when gender is 'M'" do |employee|
-  employee.gender = 'M'
-  !employee.female?
-end
-
-assert "should be true when gender is 'F'" do |employee|
-  employee.gender = 'F'
-  employee.female?
-end
-
-########
-
-puts "testing #full_name"
-
-assert "should combine first and last name" do |employee| 
-  employee.first_name = 'Bob'
-  employee.last_name = 'Smith'
+  raise "\n\n#{@label} #{message}" unless yield(employee)
   
-  employee.full_name == 'Bob Smith'
+  $stdout.print '.'; $stdout.flush
 end
 
-assert "should only use first when last is unavailable" do |employee|
-  employee.first_name = 'Bob'
-  employee.full_name == 'Bob'
+describe "#male?" do
+  assert "should be true when gender is 'M'" do |employee|
+    employee.gender = 'M'
+    employee.male?
+  end
+
+  assert "should be false when gender is 'F'" do |employee|
+    employee.gender = 'F'
+    !employee.male?
+  end
 end
 
-assert "should only use last when first is unavailable" do |employee|
-  employee.last_name = 'Smith'
-  employee.full_name == 'Smith'
+describe "#female?" do
+  assert "should be false when gender is 'M'" do |employee|
+    employee.gender = 'M'
+    !employee.female?
+  end
+
+  assert "should be true when gender is 'F'" do |employee|
+    employee.gender = 'F'
+    employee.female?
+  end
 end
 
-puts "SUCCESS!"
+describe "#full_name" do
+  assert "should combine first and last name" do |employee| 
+    employee.first_name = 'Bob'
+    employee.last_name = 'Smith'
+  
+    employee.full_name == 'Bob Smith'
+  end
+
+  assert "should only use first when last is unavailable" do |employee|
+    employee.first_name = 'Bob'
+    employee.full_name == 'Bob'
+  end
+
+  assert "should only use last when first is unavailable" do |employee|
+    employee.last_name = 'Smith'
+    employee.full_name == 'Smith'
+  end
+end
+
+puts "\n\nSUCCESS!"
