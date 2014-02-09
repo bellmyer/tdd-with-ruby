36673325574a86b19ff864ed7145bd7d2f7edd32
Jaime Bellmyer <online@bellmyer.com>
2014-02-09 17:22:23 -0600

    add employee full_name method

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,10 @@
 class Employee
-  attr_accessor :gender
+  attr_accessor :gender, :first_name, :last_name
   
   def initialize options={}
     @gender = options[:gender]
+    @first_name = options[:first_name]
+    @last_name = options[:last_name]
   end
   
   def male?
@@ -11,5 +13,9 @@ class Employee
   
   def female?
     gender == 'F'
+  end
+  
+  def full_name
+    [first_name, last_name].join(' ').strip
   end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,6 +2,8 @@ require_relative "../lib/employee"
 
 employee = Employee.new
 
+########
+
 puts "testing #male?"
 
 employee.gender = 'M'
@@ -10,6 +12,8 @@ raise "#male? should be true when gender is 'M'" unless employee.male?
 employee.gender = 'F'
 raise "#male? should be false when gender is 'F'" unless !employee.male?
 
+########
+
 puts "testing #female?"
 
 employee.gender = 'M'
@@ -17,5 +21,21 @@ raise "#female? should be false when gender is 'M'" unless !employee.female?
 
 employee.gender = 'F'
 raise "#female? should be true when gender is 'F'" unless employee.female?
+
+########
+
+puts "testing #full_name"
+
+employee.first_name = 'Bob'
+employee.last_name = 'Smith'
+raise "#full_name? should combine first and last name" unless employee.full_name == 'Bob Smith'
+
+employee.first_name = 'Bob'
+employee.last_name = nil
+raise "#full_name? should only use first when last is unavailable" unless employee.full_name == 'Bob'
+
+employee.first_name = nil
+employee.last_name = 'Smith'
+raise "#full_name? should only use last when first is unavailable" unless employee.full_name == 'Smith'
 
 puts "SUCCESS!"
